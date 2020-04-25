66573bb84d63199606ab509026997defac427f15
Nicolas 'eowfenth' Deçordi dos Reis <eowfenth.services@gmail.com>
2020-04-25 19:51:19 -0300

    feat: hello world using react

---
+++ app/App.tsx
@@ -0,0 +1,10 @@
+import { hot } from 'react-hot-loader/root'
+import * as React from 'react'
+
+const App = () => (
+  <div>
+    <h1>Hello, Serverphobia!</ h1>
+  </div>
+)
+
+export default hot(App)
