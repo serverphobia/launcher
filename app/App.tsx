ba76977dd36bc83e8b2d025b6fe7a56a3a90d49e
Nicolas 'eowfenth' Deçordi dos Reis <eowfenth.services@gmail.com>
2020-04-26 01:45:30 -0300

    feat: add hello world as inicial app.tsx

--- app/App.tsx
+++ app/App.tsx
@@ -1,10 +1,32 @@
 import { hot } from 'react-hot-loader/root'
 import * as React from 'react'
+import styled, { createGlobalStyle } from "styled-components";
 
-const App = () => (
-  <div>
-    <h1>Hello, Serverphobia!</ h1>
-  </div>
-)
+const Global = createGlobalStyle`
+  body {
+    margin: 0;
+  }
+`;
+
+const Container = styled.div`
+  display: flex;
+  height: 100vh;
+  width: 100vw;
+  background-color: rebeccapurple;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Header = styled.h1`
+  color: #FFFFFF;
+  font-size: 36px;
+`;
+
+const App = () => <>
+  <Global />
+  <Container>
+    <Header>Hello, World!</Header>
+  </Container>
+</>
 
 export default hot(App)
